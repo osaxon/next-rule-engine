b4443e46bbfcc99e7214f8a2fa13c7f8422fc7f0
Oliver Saxon <oliverrsaxon@gmail.com>
2025-07-28 14:09:09 +0100

    fixes imports

--- app/rules/[ruleName]/page.tsx
+++ app/rules/[ruleName]/page.tsx
@@ -1,11 +1,11 @@
 import { AppContainer } from "@/components/app-container";
 import { TryRule } from "@/components/try-rule-form/try-rule-form";
-import { Rules } from "@/features/rules/types";
+import { TRuleNames } from "@/features/rules/types";
 
 export default async function RulePage({
   params,
 }: {
-  params: Promise<{ ruleName: Rules }>;
+  params: Promise<{ ruleName: TRuleNames }>;
 }) {
   const { ruleName } = await params;
 
